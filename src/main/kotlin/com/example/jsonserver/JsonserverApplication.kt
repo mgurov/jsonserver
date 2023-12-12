37c65eeabac5995160e8f5b34ad0d44a241993d8
Mykola Gurov <ngurov@gmail.com>
2023-12-12 08:05:07 +0100

    initial

---
+++ src/main/kotlin/com/example/jsonserver/JsonserverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jsonserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsonserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<JsonserverApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jsonserver/JsonserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jsonserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JsonserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

b34f85847c7590be4fbe09fd82743b3ba4c29761
Mykola Gurov <ngurov@gmail.com>
2023-12-12 13:27:09 +0100

    aha, constructor combination not yet supported, got it

---
+++ src/main/kotlin/com/example/jsonserver/api/kotlinvars/Vals.kt
@@ -0,0 +1,15 @@
+package com.example.jsonserver.api.kotlinvars
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped
+
+data class Base(
+    val title: String,
+    val description: String,
+)
+
+data class Holder(
+    var details: String,
+    @JsonUnwrapped
+    var base: Base,
+)
+//TODO: move base out of the constructor

b25ffdc05f4b784b087cde007493736db7d170b5
Mykola Gurov <ngurov@gmail.com>
2023-12-20 12:43:23 +0100

    delays and such

--- src/main/kotlin/com/example/jsonserver/api/data/DataAPI.kt
+++ src/main/kotlin/com/example/jsonserver/api/data/DataAPI.kt
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 class DataAPI {
@@ -16,6 +17,8 @@ class DataAPI {
         pageSize: Int,
         @RequestParam(required = false)
         nextPage: String?,
+        @RequestParam(required = false, defaultValue = "10")
+        delay: Long,
     ) : Response {
 
         val data: List<String> = (1..size).map { it.toString() }
@@ -28,6 +31,8 @@ class DataAPI {
         } else {
             (startingFrom + dataPage.size).toString()
         }
+
+        TimeUnit.SECONDS.sleep(delay)
 
         return Response(dataPage, nextPage = nextNextPage, pageSize = pageSize)
 

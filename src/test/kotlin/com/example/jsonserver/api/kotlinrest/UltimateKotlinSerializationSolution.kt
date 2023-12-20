c3f35447497c2de741ac292f601bdee257b4e2e1
Mykola Gurov <ngurov@gmail.com>
2023-12-20 18:30:58 +0100

    interesting

--- src/test/kotlin/com/example/jsonserver/api/kotlinrest/UltimateKotlinSerializationSolution.kt
+++ src/test/kotlin/com/example/jsonserver/api/kotlinrest/UltimateKotlinSerializationSolution.kt
@@ -1,5 +1,7 @@
 package com.example.jsonserver.api.kotlinrest
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.assertj.core.api.Assertions
@@ -12,15 +14,15 @@ class UltimateKotlinSerializationSolution {
     @Test
     fun `should serialize base`() {
         val given = ReadSchema()
-        given.updateableProps = UpdateOnlyProps(
-            updatedBy = "me@here"
+        given.mutableProps = MutableProps(
+            name = "naam",
+            description = "опис"
         )
         given.createableProps = CreateOnlyProps(
             createdBy = "them@there"
         )
-        given.mutableProps = MutableProps(
-            name = "naam",
-            description = "опис"
+        given.updateableProps = UpdateOnlyProps(
+            updatedBy = "me@here"
         )
         given.readOnlyProps = ReadOnlyProps(
             createdOn = Instant.parse("2020-01-01T00:00:00Z"),
@@ -30,10 +32,33 @@ class UltimateKotlinSerializationSolution {
 
         val actual = objectMapper.writeValueAsString(given)
 
-        Assertions.assertThat(actual).isEqualTo("""{"title":"title","description":"desc"}""")
+        //language=JSON
+        Assertions.assertThat(actual).isEqualTo("""{"name":"naam","description":"опис","createdBy":"them@there","updatedBy":"me@here","createdOn":"2020-01-01T00:00:00Z","updatedOn":"2021-11-11T11:11:11Z"}""")
     }
 
     private val objectMapper = jacksonObjectMapper()
         .registerModules(JavaTimeModule())
+        .apply { disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) }
+
+
+    fun configureObjectMapper(): ObjectMapper {
+        val objectMapper = ObjectMapper()
+
+        // Register the JavaTimeModule to handle JSR 310 dates, including Instant
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(JavaTimeModule())
+
+        return objectMapper
+    }
+
+    @Test
+    fun test() {
+        val objectMapper = configureObjectMapper()
+
+        // Example: Serializing an Instant using ISO format
+        val instant = Instant.now()
+        val jsonString = objectMapper.writeValueAsString(instant)
+        println("Serialized JSON: $jsonString")
+    }
 
 }
